aadad1dd4b6a49999ec220661aeeba44f0d9c28b
IRFAN ALI.E <96883453+Irfanali0660@users.noreply.github.com>
2023-10-01 18:33:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 //cors configuration
 
 const corsoptions = {
-  origin: 'https://devdomforum.irfanali.club/#/',
+  origin: 'https://devdomforum.irfanali.club',
   methods: 'GET, POST, PUT ,DELETE,PATCH',
   allowedHeaders: 'Content-Type, Authorization',
   optionsSuccessStatus: 200
